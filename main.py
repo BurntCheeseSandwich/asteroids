3d4a9ed45c94da42fc47e7da17652c5e24cc2e6d
BurntCheeseSandwich <tostie4@hotmail.com>
2024-10-30 15:25:28 +0100

    added player

--- main.py
+++ main.py
@@ -3,19 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    
     while True:
-        screen.fill((0,0,0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+            
+        screen.fill((0,0,0))
+        player1.draw(screen)
+        pygame.display.flip()
 
+        # Limit FPS to 60
+        dt = clock.tick(60) / 1000
+        
+        
 if __name__ == "__main__":
     main()
